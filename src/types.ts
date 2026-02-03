46f11ed72942dc636a3c106a27d3cefe95b36785
Yiwei Ho <davidho0403@gmail.com>
2026-02-03 22:08:49 +0800

    feat: demo snippet

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export type Snippet = {
+  id: string;
+  text: string;
+  createdAt: number;
+  updatedAt: number;
+};
+
+export type Demo = {
+  id: string;
+  name: string;
+  snippets: Snippet[];
+  createdAt: number;
+  updatedAt: number;
+};
+
+export type ActiveState = {
+  demoId: string;
+  index: number;
+};
